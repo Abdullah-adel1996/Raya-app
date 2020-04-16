fef3b3a56d7288c3c6a756bdae1b30b45948845a
abdullahkhalil <abdullahadel19969@gmail.com>
2020-04-16 21:38:23 +0200

    final

--- src/header-section/Heading.js
+++ src/header-section/Heading.js
@@ -8,7 +8,7 @@ const Heading = props => {
         <Row>
            
             <header className='heading'>
-                <div style={{width:'47%', margin:'5% 11.5%'}}>
+                <div style={{width:'47%', margin:'7% 11.5%'}}>
                     <h1 className='headerText' style={{fontWeight:'900', fontSize:'3.5rem', marginBottom:'3rem'}}>WE ARE MORE THAN A BUSINESS</h1>
                     <p style={{fontWeight:'400', fontSize:'1.5rem', color:'#fff', marginBottom:'3rem'}}>we are more than a business, we ar an en extend family of young, open minded, ambitious and always enthusiastic experts with various background. </p>
                     <Arrow icon={faChevronCircleDown}/>
